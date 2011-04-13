8047b6c40aa4a0dd5b9061af7acd8462bfd89f0c
hughessd <steve@albertagrocery.coop>
2011-04-12 19:21:56 -0700

    Rearranged and managed new links.

--- extra/index.php
+++ extra/index.php
@@ -3,8 +3,14 @@
    $header = 'Extra Section Index';
    include ('../includes/header.html');
 ?><body>
+	<a href="/extra/owner_details.php">Owner Details Lookup</a></br>
+	Look up an owner's account details
+</br></br>
 	<a href="/extra/trans_lookup.php">Transaction Lookup</a></br>
 	Look up a previous transaction
+</br></br>
+	<a href="/extra/tender_report.php">Tender Report Lookup</a></br>
+	Look up a previous tender report
 </br></br>
 	<a href="/extra/shelftags.php">Generate Shelftags</a></br>
 	Generate shelftags
@@ -12,12 +18,14 @@
 	<a href="/extra/member_label.php">Generate Member Labels</a></br>
 	Member Labels
 </br></br>
-	<a href="/giftcert/giftcertadd.php">Gift Certificates</a><br>
+<!--	<a href="/giftcert/giftcertadd.php">Gift Certificates</a><br>
 	Play Around With Gift Certificates
-</br></br>
-	<a href="/extra/phoneList.php">Phone List Generator</a><br>
+</br></br>-->
+	<a href="/extra/phoneList.php">Generate Phone Bank List</a><br>
 	Generate Phone Bank Lists For Membership
-
+</br></br>
+	<a href="/extra/contactList.php">Generate Owner Email List</a><br>
+	Generate Email List of Membership
 </br>
 <?php
    include ('../includes/footer.html');
